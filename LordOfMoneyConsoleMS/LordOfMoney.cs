3634c1ad6958c75a6eb12c59aac2014db0048a41
OlegKzoev <67964582+OlegKzoev@users.noreply.github.com>
2021-07-17 16:17:03 +0300

    Add files via upload

---
+++ LordOfMoneyConsoleMS/LordOfMoney.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace LordOfMoneyConsole
+{
+    class LordOfMoney
+    {
+        public static decimal sum;
+        static void Main(string[] args)
+        {
+            StreamWriter sww = new StreamWriter("config.ini");//Создание файла config.ini
+            sww.Close();
+
+            /*
+            Console.WriteLine(Console.OutputEncoding.HeaderName);
+
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine(Console.OutputEncoding.HeaderName);
+            */
+
+
+            About about = new About();
+            Menu menu = new Menu();
+            Command cmd = new Command();
+
+            ConsoleKeyInfo cki;
+
+            SalaryBalance sb;            
+
+            about.OutputShort();
+            menu.ShowMenu();
+            cmd.ShowPrompt();
+
+            //
+            // переделать управление меню, чтобы отображались только цифры из меню, все остальное нельзя было напечатать
+            // см Console.ReadKey(Boolean), Console.KeyAvailable
+            do
+            {
+                cki = Console.ReadKey();
+
+                if (cki.Key == ConsoleKey.D1 | cki.Key == ConsoleKey.NumPad1)
+                {
+                    Console.WriteLine("");
+                    
+                    Console.Write("Введите остаток баланса на текущий месяц: ");
+                    
+                    decimal sum = Convert.ToDecimal(Console.ReadLine());
+                    sb = new SalaryBalance(sum);
+                    StreamWriter sw = new StreamWriter("config.ini");//Открытие "писателя"
+                    sw.WriteLine(sum);//Запись в файл
+                    sw.Close();//Закрытие "писателя"
+                    sb.PrintTable();
+                    menu.ShowMenu();
+                    cmd.ShowPrompt();
+                }
+
+                if (cki.Key == ConsoleKey.D3 | cki.Key == ConsoleKey.NumPad3)
+                {
+                    Console.WriteLine("");
+                    about.OutputLong();
+                    menu.ShowMenu();
+                    cmd.ShowPrompt();
+                }
+
+            } while (cki.Key != ConsoleKey.D4 & cki.Key != ConsoleKey.NumPad4);
+        }
+    }
+
+
+}
---
+++ LordOfMoneyConsoleMS/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace LordOfMoneyConsole
+{
+    class Menu
+    {
+        string line;//Переменная, в которую будут записываться данные
+        public Menu()
+        {
+
+        }
+
+        public void ShowMenu()
+        {
+            StreamReader sr = new StreamReader("config.ini");//Открытие ридера
+            line = sr.ReadLine();//Запись данных с файла в переменную line
+            while (line != null)//Цикл который проверяет, если переменная не пуста, тогда отображается последний введённый баланс
+            {
+                Console.WriteLine("Последний введённый вами баланс: " + line);
+                line = sr.ReadLine();
+            }
+            sr.Close();//Закрытие ридера
+
+            Console.WriteLine("1. Ввести остаток баланс на текущий месяц/Input salary balance");
+            Console.WriteLine("2. ");
+            Console.WriteLine("3. О программе/About");
+            Console.WriteLine("4. Выход/Exit");
+        }
+    }    
+}
